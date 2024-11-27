097cfaa48d17a0624d766d3cc526b76dc308a0f1
ZeynepHasrett <158554030+ZeynepHasrett@users.noreply.github.com>
2024-11-27 13:24:37 +0300

    (day04)

---
+++ src/day04_relativeLocators/C01_RelativeLocators.java
@@ -0,0 +1,33 @@
+package day04_relativeLocators;
+
+public class C01_RelativeLocators {
+
+    public static void main(String[] args) {
+
+        // 1 ) https://testotomasyonu.com/relativeLocators adresine gidin
+
+
+        // 2 ) DSLR Camera’yi etrafindaki elementleri kullanarak 3 farkli relative locator ile locate edip tiklayin
+
+                // 1- Relative Locator'i kullanmak icin oncelikle
+                //    hedef webelemente ait bir ozelligi bulmamiz gerekir
+                //    bunun icin locate edecek gibi webelementi inceleyelim
+
+
+                // 2- Hedef webelementin etrafinda kullanabildigimiz bir webelementi
+                //    locate edip kaydedelim
+
+
+                // 3- Artik Relative locator elementi locate edip kaydedebiliriz
+
+
+
+        // 3 ) Acilan urunun DSLR Camera oldugunu test edin.
+
+
+        // 4 ) Sayfayi kapatin
+
+
+    }
+
+}
